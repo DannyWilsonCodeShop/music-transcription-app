970e7528d71a0475374d26d54d5c13f01feeed2a
Danny Wilson <dannywilson@example.com>
2026-01-24 21:19:55 -0500

    Temporarily remove Lambda functions to fix deployment
    The Lambda functions are causing build failures. Removing them temporarily
    to get the frontend deployed. The backend pipeline will be deployed separately
    using the CloudFormation template and deploy scripts.
    
    This allows the UI to work while we troubleshoot the Lambda integration.

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,11 +2,6 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
-import { transcribeAudio } from './functions/transcribe-audio/resource';
-import { jobTrigger } from './functions/job-trigger/resource';
-import { Policy, PolicyStatement } from 'aws-cdk-lib/aws-iam';
-import { StartingPosition } from 'aws-cdk-lib/aws-lambda';
-import { DynamoEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
 
 /**
  * Music Transcription App Backend
@@ -16,46 +11,4 @@ const backend = defineBackend({
   auth,
   data,
   storage,
-  transcribeAudio,
-  jobTrigger,
 });
-
-// Grant the transcribe function permissions to access DynamoDB and Step Functions
-const transcribeFunction = backend.transcribeAudio.resources.lambda;
-transcribeFunction.addToRolePolicy(
-  new PolicyStatement({
-    actions: ['dynamodb:PutItem', 'dynamodb:GetItem', 'dynamodb:UpdateItem'],
-    resources: ['*'], // Will be restricted to specific table in production
-  })
-);
-transcribeFunction.addToRolePolicy(
-  new PolicyStatement({
-    actions: ['states:StartExecution'],
-    resources: ['*'], // Will be restricted to specific state machine in production
-  })
-);
-
-// Grant the job trigger function permissions to start Step Functions
-const jobTriggerFunction = backend.jobTrigger.resources.lambda;
-jobTriggerFunction.addToRolePolicy(
-  new PolicyStatement({
-    actions: ['states:StartExecution'],
-    resources: ['*'], // Will be restricted to specific state machine in production
-  })
-);
-
-// Add DynamoDB Stream trigger to job trigger function
-// Note: This requires the DynamoDB table to have streams enabled
-// The stream ARN will need to be configured after deployment
-const dataResources = backend.data.resources;
-const jobsTable = dataResources.tables['TranscriptionJob'];
-
-if (jobsTable) {
-  jobTriggerFunction.addEventSource(
-    new DynamoEventSource(jobsTable, {
-      startingPosition: StartingPosition.LATEST,
-      batchSize: 10,
-      retryAttempts: 3,
-    })
-  );
-}
